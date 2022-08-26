a79db428f89a730aa533ce34b1c65906cf54738e
Martin Dobrev <martindobrev87@gmail.com>
2022-08-26 15:38:14 +0300

    Formatting updated

--- scripts/05-multireward-interactions.ts
+++ scripts/05-multireward-interactions.ts
@@ -1,36 +1,39 @@
 // @ts-nocheck
 import hardhat from 'hardhat';
 async function deployMultiRewardsFromFactory(campaignAddress: string, walletAddress: string) {
-  const MultiRewardsContract = await hardhat.hethers.getContractAt('MultiRewards', campaignAddress);
+    const MultiRewardsContract = await hardhat.hethers.getContractAt(
+        'MultiRewards',
+        campaignAddress,
+    );
 
-  console.log('⚙️ Calling Multirewards contract ...');
+    console.log('⚙️ Calling Multirewards contract ...');
 
-  const multiRewardsContractReward = await MultiRewardsContract.rewardTokens(0);
-  const multiRewardsContractUserBalance = await MultiRewardsContract.balanceOf(walletAddress);
-  const multiRewardsContractTotalSupply = await MultiRewardsContract.totalSupply();
-  const multiRewardsContractRewardEarned = await MultiRewardsContract.earned(
-    walletAddress,
-    multiRewardsContractReward,
-  );
-  const multiRewardsContractRewardDuration = await MultiRewardsContract.getRewardForDuration(
-    multiRewardsContractReward,
-  );
+    const multiRewardsContractReward = await MultiRewardsContract.rewardTokens(0);
+    const multiRewardsContractUserBalance = await MultiRewardsContract.balanceOf(walletAddress);
+    const multiRewardsContractTotalSupply = await MultiRewardsContract.totalSupply();
+    const multiRewardsContractRewardEarned = await MultiRewardsContract.earned(
+        walletAddress,
+        multiRewardsContractReward,
+    );
+    const multiRewardsContractRewardDuration = await MultiRewardsContract.getRewardForDuration(
+        multiRewardsContractReward,
+    );
 
-  const multiRewardsContractRewardData = await MultiRewardsContract.rewardData(
-    multiRewardsContractReward,
-  );
+    const multiRewardsContractRewardData = await MultiRewardsContract.rewardData(
+        multiRewardsContractReward,
+    );
 
-  console.log('✅ Reward address:', multiRewardsContractReward);
-  console.log('✅ User balance:', multiRewardsContractUserBalance.toString());
-  console.log('✅ Total supply:', multiRewardsContractTotalSupply.toString());
-  console.log('✅ Reward earned:', multiRewardsContractRewardEarned.toString());
-  console.log('✅ Reward duration:', multiRewardsContractRewardDuration.toString());
-  console.log('✅ Reward rewardsDistributor:', multiRewardsContractRewardData[0].toString());
-  console.log('✅ Reward rewardsDuration:', multiRewardsContractRewardData[1].toString());
-  console.log('✅ Reward periodFinish:', multiRewardsContractRewardData[2].toString());
-  console.log('✅ Reward rewardRate:', multiRewardsContractRewardData[3].toString());
-  console.log('✅ Reward lastUpdateTime:', multiRewardsContractRewardData[4].toString());
-  console.log('✅ Reward rewardPerTokenStored:', multiRewardsContractRewardData[5].toString());
+    console.log('✅ Reward address:', multiRewardsContractReward);
+    console.log('✅ User balance:', multiRewardsContractUserBalance.toString());
+    console.log('✅ Total supply:', multiRewardsContractTotalSupply.toString());
+    console.log('✅ Reward earned:', multiRewardsContractRewardEarned.toString());
+    console.log('✅ Reward duration:', multiRewardsContractRewardDuration.toString());
+    console.log('✅ Reward rewardsDistributor:', multiRewardsContractRewardData[0].toString());
+    console.log('✅ Reward rewardsDuration:', multiRewardsContractRewardData[1].toString());
+    console.log('✅ Reward periodFinish:', multiRewardsContractRewardData[2].toString());
+    console.log('✅ Reward rewardRate:', multiRewardsContractRewardData[3].toString());
+    console.log('✅ Reward lastUpdateTime:', multiRewardsContractRewardData[4].toString());
+    console.log('✅ Reward rewardPerTokenStored:', multiRewardsContractRewardData[5].toString());
 }
 
 module.exports = deployMultiRewardsFromFactory;
