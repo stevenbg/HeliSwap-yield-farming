e4ff69d503254b42cf2eb20916b3bc3851f93343
Martin Dobrev <martindobrev87@gmail.com>
2023-09-18 10:34:48 +0300

    Scripts for testnet updated

--- scripts/01-deploy-factory.ts
+++ scripts/01-deploy-factory.ts
@@ -3,13 +3,16 @@ import hardhat from 'hardhat';
 
 // Canonical WHBAR Address on Testnet
 // Reference https://github.com/LimeChain/whbar
-const WHBAR_ADDRESS = '0x0000000000000000000000000000000000001c3D';
+const WHBAR_ADDRESS = '0x000000000000000000000000000000000006A6cB'; // Testnet WHBAR
+const POOL_FACTORY = '0x000000000000000000000000000000000006a74f'; // Testnet DEX factory
 
 async function deployFactory() {
   const Factory = await hardhat.hethers.getContractFactory('Factory');
+  const fee = 0;
+  const feeAddress = '0x61c90019e9fb0d95cbd39cb68e0b4f217526e2da';
 
   console.log('⚙️ Deploying factory contract...');
-  const factory = await Factory.deploy(WHBAR_ADDRESS);
+  const factory = await Factory.deploy(WHBAR_ADDRESS, fee, feeAddress, POOL_FACTORY);
   await factory.deployed();
 
   console.log('✅ MultiRewards Factory contract deployed to:', factory.address);
